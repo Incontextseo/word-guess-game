0fe6da66065a2b32074c9b40cd3fe82c5c8d04cb
unknown <incontextseo@gmail.com>
2019-04-20 20:54:24 -0500

    game working, made it look pretty.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 <script>
 
-    // Creates an array of possible words for word-guess-game //
+// Creates an array of possible words for word-guess-game //
     
     var tvShows = [
         "friends",
@@ -24,14 +24,23 @@
     var rightLetter = [];
     var wrongLetter = [];
     var underscore = [];
-    var guessesLeft = 10;
+    var remainingGuess = 10;
+    var winsTotal = 0;
+    var winnerText = "";
+    var usedLetter = [];
     
+    var winsTotalText = document.getElementById("wins");
+    var underscoreText = document.getElementById("word");
+    var wrongLetterText = document.getElementById("wrong-letter");
     var guessesLeftText = document.getElementById("guesses");
+    var resetButton = document.getElementById("reset");
+    var winnerText = document.getElementById("winner");
 
     console.log(show)
     
     // Creates underscores based on length of word //
     var makeUnderscore = () => {
+        underscore = []
         for (var i=0; i<show.length; i++) {
             underscore.push(" _ ");
         }
@@ -46,34 +55,72 @@
     document.onkeyup = function() {
         var userGuess = String.fromCharCode(event.keyCode).
         toLowerCase();
-    
-    // Tells the user how many guesses they have remaining out of 10 //
-        
-        guessesLeft--
-        console.log(guessesLeft);
+      
     
     // Determines if userGuess is found in word, pushes to rightLetter or wrongLetter array //   
-    
+    console.log(userGuess)
         if(show.indexOf(userGuess) > -1) {
             for (var w = 0; w < show.length; w++) {
                 if (show[w] === userGuess) {
     
     // Replaces underscores with correct user guesses //
-                    underscore[show.indexOf(userGuess)] = userGuess;
+                    underscore[w] = userGuess;
                     rightLetter.push(userGuess);
                     console.log(show[w])
-                }}
-                    console.log("In word")
-                    console.log(show.indexOf(userGuess))
+                    console.log("In word");
+                    console.log(show.indexOf(userGuess));
+                    console.log(rightLetter);
                     console.log(underscore);
+
+                }
+
             }
-            else{
+        } else if (wrongLetter.includes(userGuess)) {
+            usedLetter.push(userGuess);
+            console.log(usedLetter);
+        } else {
             wrongLetter.push(userGuess);
-                    console.log("Not in word")
+            remainingGuess--;
+                    console.log(wrongLetter);   
+                    console.log("Not in word");
         }
-    
-    };
 
-    guessesLeftText.textContent = guessesLeft;
+        console.log(typeof remainingGuess)
+        guessesLeftText.textContent = remainingGuess;
+        if (remainingGuess < 1) {
+            console.log(typeof remainingGuess)
+                guessesLeftText.innerHTML = "Game Over";
+                console.log("Game Over")
+ 
+        }
+        
+        console.log("underscoreText=" + underscore.join(''))
+        if (underscore.join('') === show) {
+            winnerText.innerHTML = "You Won!";
+            winsTotal++;
+                console.log(winsTotal)
+                console.log("underscoreText=" + underscore.join(''))
 
-    </script>
+        }
+        // Moving all elements to the DOM
+        winsTotalText.textContent = winsTotal;
+        underscoreText.textContent = underscore.join(' ');
+        wrongLetterText.textContent = wrongLetter;
+
+    }
+
+    resetButton.addEventListener("click", function(){
+        show = ""
+        show = tvShows[Math.floor(Math.random() * tvShows.length)];
+        console.log(show)
+        guessesLeftText.textContent = 10;
+        usedLetter = []
+        wrongLetter = []
+        winnerText.innerHTML = '';
+        wrongLetterText.innerHTML = [];
+        makeUnderscore();
+        underscoreText.textContent = underscore.join(' ');
+    })
+
+    underscoreText.textContent = underscore.join(' ');
+</script>
