c713e0c4fbb8e0fa6c9882ccf90c7761545a83a6
unknown <incontextseo@gmail.com>
2019-05-20 13:05:12 -0500

    Put game into responsive design format, need to fix the navbar and game function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-<script>
-
-// Creates an array of possible words for word-guess-game //
+    // Creates an array of possible words for word-guess-game //
     
     var tvShows = [
         "friends",
@@ -15,6 +13,8 @@
         "er",
         "roseanne",
         "arthur",
+        "fresh prince",
+        "family matters",
     ]
     
     
@@ -28,6 +28,8 @@
     var winsTotal = 0;
     var winnerText = "";
     var usedLetter = [];
+    var timer;
+    var keyup = true;
     
     var winsTotalText = document.getElementById("wins");
     var underscoreText = document.getElementById("word");
@@ -35,27 +37,49 @@
     var guessesLeftText = document.getElementById("guesses");
     var resetButton = document.getElementById("reset");
     var winnerText = document.getElementById("winner");
+    var directionsText = document.getElementById("directions-text")
 
     console.log(show)
     
     // Creates underscores based on length of word //
     var makeUnderscore = () => {
+        clearInterval(timer);
         underscore = []
-        for (var i=0; i<show.length; i++) {
-            underscore.push(" _ ");
-        }
+            for (var i=0; i<show.length; i++) {
+                if (show[i] === " ") {
+                underscore.push("*");
+                } else {
+                underscore.push("_");
+                }
+            }
         return underscore;
         }
     
     console.log(makeUnderscore());
     
-    
+    function restartGame () {
+        keyup = true;
+        clearInterval(timer);
+        show = ""
+        show = tvShows[Math.floor(Math.random() * tvShows.length)];
+        console.log(show)
+        remainingGuess = 10;
+        guessesLeftText.textContent = remainingGuess;
+        usedLetter = []
+        wrongLetter = []
+        winnerText.innerHTML = '';
+        wrongLetterText.innerHTML = [];
+        makeUnderscore();
+        underscoreText.textContent = underscore.join(' ');
+    }
+
+if (keyup === true) {
     // Record keys user presses //
     
     document.onkeyup = function() {
         var userGuess = String.fromCharCode(event.keyCode).
         toLowerCase();
-      
+    
     
     // Determines if userGuess is found in word, pushes to rightLetter or wrongLetter array //   
     console.log(userGuess)
@@ -91,13 +115,16 @@
             console.log(typeof remainingGuess)
                 guessesLeftText.innerHTML = "Game Over";
                 console.log("Game Over")
- 
+                timer = setInterval(function(){
+                    restartGame();
+                  }, 5000);
         }
         
         console.log("underscoreText=" + underscore.join(''))
         if (underscore.join('') === show) {
             winnerText.innerHTML = "You Won!";
             winsTotal++;
+            keyup = false;
                 console.log(winsTotal)
                 console.log("underscoreText=" + underscore.join(''))
 
@@ -110,17 +137,12 @@
     }
 
     resetButton.addEventListener("click", function(){
-        show = ""
-        show = tvShows[Math.floor(Math.random() * tvShows.length)];
-        console.log(show)
-        guessesLeftText.textContent = 10;
-        usedLetter = []
-        wrongLetter = []
-        winnerText.innerHTML = '';
-        wrongLetterText.innerHTML = [];
-        makeUnderscore();
-        underscoreText.textContent = underscore.join(' ');
+        restartGame ();
     })
+}
 
+    // // Hide the directions
+    // directionsText.textContent = "";
+
+    // Populate TV Show to guess   
     underscoreText.textContent = underscore.join(' ');
-</script>
